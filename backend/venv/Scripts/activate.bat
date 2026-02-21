471d10109de26bd5474f5215b8497d722e844142
SyedAbdulSami1 <samiwpp@gmail.com>
2026-02-21 09:45:46 +0500

    work for chatbot

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\D Data\Hackthon-Second\hackathon-todo-phase-II\backend\venv"
+set "VIRTUAL_ENV=D:\D Data\Hackthon-Second\hackathon-todo-phase-III\backend\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
